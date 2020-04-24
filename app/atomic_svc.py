5b66cb5c9d4501a937dfc052d4d0febc7e3fb232
zaphod <zaphod@eleves.polytechnique.fr>
2020-04-24 16:21:39 +0200

    Fix misc. issues + README + count abilities imported

--- app/atomic_svc.py
+++ app/atomic_svc.py
@@ -15,33 +15,29 @@ from app.utility.base_service import BaseService
 PLATFORMS = dict(windows='windows', macos='darwin', linux='linux')
 EXECUTORS = dict(command_prompt='cmd', sh='sh', powershell='psh', bash='sh')
 RE_VARIABLE = re.compile('(#{(.*?)})', re.DOTALL)
+PREFIX_HASH_LEN = 6
 
 
 class AtomicService(BaseService):
+
     def __init__(self, services):
         self.log = self.add_service('atomic_svc', self)
 
         # Atomic Red Team attacks don't come with the corresponding tactic (phase name)
         # so we need to create a match between techniques and tactics.
         # This variable is filled by self._populate_dict_techniques_tactics()
-        self.technique_to_tactics = None
+        self.technique_to_tactics = defaultdict(list)
 
         self.atomic_dir = os.path.join('plugins', 'atomic')
         self.repo_dir = os.path.join(self.atomic_dir, 'atomic-red-team')
         self.data_dir = os.path.join(self.atomic_dir, 'data')
         self.payloads_dir = os.path.join(self.atomic_dir, 'payloads')
 
-        # abilities ingested are counted in term of platform blocks created
-        self.at_ingested = 0
-        self.at_total = 0
-
-        self.errors = 0
-
     async def clone_atomic_red_team_repo(self, repo_url=None):
-        '''
+        """
         Clone the Atomic Red Team repository. You can use a specific url via
         the `repo_url` parameter (eg. if you want to use a fork).
-        '''
+        """
         if not repo_url:
             repo_url = 'https://github.com/redcanaryco/atomic-red-team.git'
 
@@ -49,39 +45,42 @@ class AtomicService(BaseService):
             check_call(['git', 'clone', '--depth', '1', repo_url, self.repo_dir], stdout=DEVNULL, stderr=STDOUT)
 
     async def populate_data_directory(self, path_yaml=None):
-        '''
+        """
         Populate the 'data' directory with the Atomic Red Team abilities.
         These data will be usable by caldera after importation.
         You can specify where the yaml files to import are located with the `path_yaml` parameter.
         By default, read the yaml files in the atomics/ directory inside the Atomic Red Team repository.
-        '''
+        """
         if not self.technique_to_tactics:
             await self._populate_dict_techniques_tactics()
 
         if not path_yaml:
             path_yaml = os.path.join(self.repo_dir, 'atomics', '**', 'T*.yaml')
 
+        at_total = 0
+        at_ingested = 0
+        errors = 0
         for filename in glob.iglob(path_yaml):
             for entries in BaseWorld.strip_yml(filename):
                 for test in entries['atomic_tests']:
+                    at_total += 1
                     try:
-                        incr_ingested, incr_total = await self._save_ability(entries, test)
-                        self.at_ingested += incr_ingested
-                        self.at_total += incr_total
+                        if await self._save_ability(entries, test):
+                            at_ingested += 1
                     except Exception as e:
                         self.log.debug('ERROR:', filename, e)
-                        self.errors += 1
+                        errors += 1
 
-        errors_output = f' and ran into {self.errors} errors' if self.errors else ''
-        self.log.debug(f'Ingested {self.at_ingested} abilities (out of {self.at_total}) from Atomic plugin{errors_output}')
+        errors_output = f' and ran into {errors} errors' if errors else ''
+        self.log.debug(f'Ingested {at_ingested} abilities (out of {at_total}) from Atomic plugin{errors_output}')
 
     """ PRIVATE """
 
     def _gen_single_match_tactic_technique(self, mitre_json):
-        '''
+        """
         Generator parsing the json from 'enterprise-attack.json',
         and returning couples (phase_name, external_id)
-        '''
+        """
         for obj in mitre_json.get('objects'):
             if not obj.get('type') == 'attack-pattern':
                 continue
@@ -93,20 +92,16 @@ class AtomicService(BaseService):
                     if not kc.get('kill_chain_name') == 'mitre-attack':
                         continue
                     phase_name = kc.get('phase_name')
-                    yield (phase_name, external_id)
+                    yield phase_name, external_id
 
-    async def _populate_dict_techniques_tactics(self, entreprise_attack_path=None):
-        '''
+    async def _populate_dict_techniques_tactics(self):
+        """
         Populate internal dictionary used to match techniques to corresponding tactics.
-        By default, use the file 'enterprise-attack.json' located in the Atomic Red Team repository.
-        You can use a specific version of this file by passing its path with the `entreprise_attack_path`
-        parameter.
-        '''
-        self.technique_to_tactics = defaultdict(list)
-        if not entreprise_attack_path:
-            entreprise_attack_path = os.path.join(self.repo_dir, 'atomic_red_team', 'enterprise-attack.json')
+        Use the file 'enterprise-attack.json' located in the Atomic Red Team repository.
+        """
+        enterprise_attack_path = os.path.join(self.repo_dir, 'atomic_red_team', 'enterprise-attack.json')
 
-        with open(entreprise_attack_path, 'r') as f:
+        with open(enterprise_attack_path, 'r') as f:
             mitre_json = json.load(f)
 
         for phase_name, external_id in self._gen_single_match_tactic_technique(mitre_json):
@@ -118,7 +113,7 @@ class AtomicService(BaseService):
         # to avoid collisions between payloads with the same name
         with open(attachment_path, 'rb') as f:
             h = hashlib.md5(f.read()).hexdigest()
-        payload_name = h[:6] + '_' + payload_name
+        payload_name = h[:PREFIX_HASH_LEN] + '_' + payload_name
         shutil.copyfile(attachment_path, os.path.join(self.payloads_dir, payload_name), follow_symlinks=False)
         return payload_name
 
@@ -126,55 +121,58 @@ class AtomicService(BaseService):
     def _normalize_path(path, platform):
         if platform == PLATFORMS['windows']:
             return path.replace('\\', '/')
-        else:
-            return path
+        return path
 
-    def _catch_path_to_atomics_folder(self, string, platform):
-        '''
-        Catch a path to the the atomics/ folder, and handle it in the best way
-        possible. If needed, will import a payload.
-        '''
+    def _catch_path_to_atomics_folder(self, string_to_analyse, platform):
+        """
+        Catch a path to the atomics/ folder in the `string_to_analyse` variable,
+        and handle it in the best way possible. If needed, will import a payload.
+        """
         regex = re.compile(r'\$?PathToAtomicsFolder((?:/[^/ \n]+)+|(?:\\[^\\ \n]+)+)')
         payloads = []
-        if regex.search(string):
-            fullpath, path = regex.search(string).group(0, 1)
+        if regex.search(string_to_analyse):
+            fullpath, path = regex.search(string_to_analyse).group(0, 1)
             path = self._normalize_path(path, platform)
 
             # take path from index 1, as it starts with /
             path = os.path.join(self.repo_dir, 'atomics', path[1:])
 
-            # TODO handle folders
             if os.path.isfile(path):
                 payload_name = self._handle_attachment(path)
                 payloads.append(payload_name)
-                string = string.replace(fullpath, payload_name)
+                string_to_analyse = string_to_analyse.replace(fullpath, payload_name)
 
-        return string, payloads
+        return string_to_analyse, payloads
 
-    def _use_default_inputs(self, entries, test, platform, string):
-        payloads = []  # payloads induced by a variable
+    def _use_default_inputs(self, test, platform, string_to_analyse):
+        """
+        Look if variables are used in `string_to_analyse`, and if any variable was given
+        a default value, use it.
+        """
+        payloads = []
         defaults = dict((key, val) for key, val in test.get('input_arguments', dict()).items())
-        while RE_VARIABLE.search(string):
-            full_var_string, varname = RE_VARIABLE.search(string).groups()
+        while RE_VARIABLE.search(string_to_analyse):
+            full_var_str, varname = RE_VARIABLE.search(string_to_analyse).groups()
             default_var = str(defaults.get(varname, dict()).get('default'))
 
-            if default_var:  # we found the default value of a variable
+            if default_var:
                 default_var, new_payloads = self._catch_path_to_atomics_folder(default_var, platform)
                 payloads.extend(new_payloads)
-                string = string.replace(full_var_string, default_var)
+                string_to_analyse = string_to_analyse.replace(full_var_str, default_var)
 
-        return string, payloads
+        return string_to_analyse, payloads
 
-    def _handle_multiline_commands(self, cmd):
+    @staticmethod
+    def _handle_multiline_commands(cmd):
         return cmd.replace('\n', ';')
 
     async def _prepare_cmd(self, entries, test, platform, cmd):
-        '''
+        """
         Handle a command or a cleanup (both are formatted the same way), given in `cmd`.
         Return the cmd formatted as needed and payloads we need to take into account.
-        '''
+        """
         payloads = []
-        cmd, new_payloads = self._use_default_inputs(entries, test, platform, cmd)
+        cmd, new_payloads = self._use_default_inputs(test, platform, cmd)
         payloads.extend(new_payloads)
         cmd, new_payloads = self._catch_path_to_atomics_folder(cmd, platform)
         payloads.extend(new_payloads)
@@ -182,9 +180,9 @@ class AtomicService(BaseService):
         return cmd, payloads
 
     async def _prepare_executor(self, entries, test, platform):
-        '''
+        """
         Prepare the command and cleanup, and return them with the needed payloads.
-        '''
+        """
         payloads = []
 
         command, payloads_command = await self._prepare_cmd(entries, test, platform, test['executor']['command'])
@@ -192,15 +190,13 @@ class AtomicService(BaseService):
         payloads.extend(payloads_command)
         payloads.extend(payloads_cleanup)
 
-        # TODO handle ART local files in command
-        # eg. https://github.com/redcanaryco/atomic-red-team/blob/a956d4640f9186a7bd36d16a63f6d39433af5f1d/atomics/T1022/T1022.yaml
-        # eg. https://github.com/redcanaryco/atomic-red-team/blob/fa67d8f04188955ec2c3d1532d2212f26ef5e3e8/atomics/T1502/T1502.yaml
-        return (command, cleanup, payloads)
+        return command, cleanup, payloads
 
     async def _save_ability(self, entries, test):
+        """
+        Return True iif an ability was saved.
+        """
         ability_id = hashlib.md5(json.dumps(test).encode()).hexdigest()
-        at_ingested = 0
-        at_total = 0
 
         tactics_li = self.technique_to_tactics.get(entries['attack_technique'], ['redcanary-unknown'])
         tactic = 'multiple' if len(tactics_li) > 1 else tactics_li[0]
@@ -217,7 +213,6 @@ class AtomicService(BaseService):
             platforms=dict()
         )
         for p in test['supported_platforms']:
-            at_total += 1
             if test['executor']['name'] != 'manual':
                 # manual tests are expected to be run manually by a human, no automation is provided
                 command, cleanup, payloads = await self._prepare_executor(entries, test, p)
@@ -227,8 +222,6 @@ class AtomicService(BaseService):
                 data['platforms'][platform] = dict()
                 data['platforms'][platform][executor] = dict(command=command, payloads=payloads, cleanup=cleanup)
 
-                at_ingested += 1
-
         if data['platforms']:  # this might be empty, if so there's nothing useful to save
             d = os.path.join(self.data_dir, 'abilities', tactic)
             if not os.path.exists(d):
@@ -236,5 +229,6 @@ class AtomicService(BaseService):
             file_path = os.path.join(d, '%s.yml' % ability_id)
             with open(file_path, 'w') as f:
                 f.write(yaml.dump([data]))
+            return True
 
-        return (at_ingested, at_total)
+        return False
