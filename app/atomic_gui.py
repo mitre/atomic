f77b0836351e629f59f581c04efc360476e85a51
Anna Lin <alin@mitre.org>
2021-05-05 13:31:40 -0700

    Import BaseWorld in AtomicGUI

--- app/atomic_gui.py
+++ app/atomic_gui.py
@@ -3,10 +3,11 @@ import logging
 from aiohttp_jinja2 import template
 
 from app.service.auth_svc import for_all_public_methods, check_authorization
+from app.utility.base_world import BaseWorld
 
 
 @for_all_public_methods(check_authorization)
-class AtomicGUI:
+class AtomicGUI(BaseWorld):
 
     def __init__(self, services, name, description):
         self.auth_svc = services.get('auth_svc')
