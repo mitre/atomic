adf5173c8b531a9cc8dc65d1c5dec77ec4cb0193
Caleb R Little <CLITTLE@MITRE.ORG>
2021-09-22 13:49:22 -0400

    Clean things up a bit

--- app/parsers/powershell.py
+++ app/parsers/powershell.py
@@ -1,5 +1,5 @@
 import logging
-from app.utility.base_parser import BaseParser
+from app.utility.base_parser import BaseParser, PARSER_SIGNALS_FAILURE
 
 
 class Parser(BaseParser):
@@ -9,5 +9,5 @@ class Parser(BaseParser):
             if 'FullyQualifiedErrorId' in l:
                 log = logging.getLogger('parsing_svc')
                 log.warning('This ability failed for some reason. Manually updating the link to report a failed state.')
-                return 418  # Universal Teapot error code
+                return [PARSER_SIGNALS_FAILURE]
         return []
