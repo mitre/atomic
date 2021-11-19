fbd8cf5d3b7dd34a0a4a582e1a8415559ed11d66
Adam Gaudreau <agaudreau@mitre.org>
2021-11-19 11:30:09 -0500

    Update to new ui

--- app/atomic_gui.py
+++ app/atomic_gui.py
@@ -12,12 +12,9 @@ class AtomicGUI(BaseWorld):
     def __init__(self, services, name, description):
         self.auth_svc = services.get('auth_svc')
         self.data_svc = services.get('data_svc')
-        self.name = name
-        self.description = description
 
         self.log = logging.getLogger('atomic_gui')
 
     @template('atomic.html')
     async def splash(self, request):
-        abilities = [a for a in await self.data_svc.locate('abilities') if await a.which_plugin() == 'atomic']
-        return dict(name=self.name, description=self.description, abilities=abilities)
+        return dict()
